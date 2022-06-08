b3c36225da81cf7cdd5907e1acf6610b2c18f2e5
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-06-08 11:00:56 -0400

    second

--- optimal_change.py
+++ optimal_change.py
@@ -1,42 +1,106 @@
+from collections import Counter
 import math
+
 # write a function taking in two arguments: item_cost, amount_paid
 # return a string describing the least amount of change possible
 # convert item_cost and amount_paid to pennies
 # create a remainder variable and get remainder of amount_paid-item_cost
-# initialize empy answer
-# find the largest denomination smaller than remainder
-# add found denomination to answer
+# initialize empty answer
+# loop through list and find the largest denominations smaller than remainder
+# add found denominations to answer
 # if remainder = 0, convert answer denominations into correct values
 
 def optimal_change(item_cost, amount_paid):
     if item_cost <= 0:
         return 'No change!'
+    if item_cost == amount_paid:
+        return 'No change!'
+    if item_cost > amount_paid:
+        return 'No change!'
     else:
+        change = []
         answer = []
+        output = f'The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is '
+
         amount_paid = amount_paid * 100
         item_cost = item_cost * 100
         remainder = amount_paid - item_cost
+       
+        pennies_list = [10000, 5000, 1000, 500, 100, 25, 10, 1]
+
+        # get correct change
+        for i in pennies_list:
+            while remainder:
+                if i <= remainder:
+                    change.append(i)
+                    remainder -= i
+                else:
+                    break
         
-        change = [1, 10, 25, 100, 500, 1000, 2000, 5000, 10000]
-        print(remainder)
+        # print(change) = something like [100, 100, 100, 10, 1, 1, 1, 1, 1, 1, 1, 1].
+        
+        
+        pennies = {
+            '$100 bill': 10000,
+            '$50 bill': 5000,
+            '$10 bill': 1000,
+            '$5 bill': 500,
+            '$1 bill': 100,
+            'quarter': 25,
+            'dime': 10,
+            'penny': 1
+        }
+
+        # loop through the change list and the pennies dictionary to match the correct change with the correct string.
+        for i in change:
+            for key in pennies:
+                if i == pennies[key]:
+                    answer.append(key)
+                    counts = {item:answer.count(item) for item in answer}
+        
+        # print(counts) = something like {'$1 bill': 3, 'dime': 1, 'penny': 8}.
+        
+        # loop through dictionary and put it into the output string
+        for key, value in counts.items():
+            if key != list(counts.keys())[-1] and value > 1:
+                output = output + f'{value} {key}s, '
+            elif key != list(counts.keys())[-1]:
+                output = output + f'{value} {key}, '
+            elif list(counts.keys()) and value > 1:
+                output = output + f'and {value} {key}s.'
+            else:
+                output = output + f'and {value} {key}.'
     
-        i = len(change) - 1
+    return(output)
         
-        while i > 0:
-            if remainder >= change[i]:
-                remainder -= change[i]
-                i.repeat(answer.append(change[i]))
-            i -= 1
+# optimal_change(56.82, 60)
         
-        print(remainder)
-        print(answer)
-        # for i in range(len(answer)):
-            
-
-
-
-optimal_change(1.50, 5)
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # count = 0
+        
+        # for i in answer:
+        #     if answer[0] == answer[1]:
+        #         count = count + 1
+        
+       
+       
+        # for i in pennies_list: 
+        #     if i in currency:
+        #         print(str(currency[i]))
+        #     else:
+        #         return False
+        
 
  # for key in change.keys():
     #     if math.floor(remainder/change[key]):
@@ -46,3 +110,17 @@ optimal_change(1.50, 5)
 
     #         print(answer)
     #         print(remainder)
+
+    # for i in range(len(change)):
+    #         if remainder > change[i]:
+    #             remainder = change[i] - remainder
+
+    # for values in pennies:
+    #     while remainder > pennies[values]:
+    #         if values in answer:
+    #             answer[values] += 1
+                
+    #         else:
+    #             answer[values] = 1
+    #         remainder -= pennies[values]
+       
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,9 +1,9 @@
 from optimal_change import optimal_change
 
 print(optimal_change(75.75, 100) == "The optimal change for an item that costs $75.75 with an amount paid of $100 is 1 $20 bill, 4 $1 bills and 1 quarter.")
-print(optimal_change(56.82, 60) == "The optimal change for an item that costs $56.82 with an amount paid of $60 is 2 $20 bills, 1 $10 bill, 1 $5 bill, 1 $1 bill, 3 quarters, 1 nickel and 2 pennies.")
-print(optimal_change(3.50, 10) == "The optimal change for an item that costs $3.50 with an amount paid of $10 is 1 $5 bill, 1 $1 bill, and 2 quarters.")
-print(optimal_change(26.90, 30) == "The optimal change for an item that costs $26.90 with an amount paid of $30 is 3 $1 bills and 1 dime.")
-print(optimal_change(.5), 3) == "The optimal change for an item that costs $.5 with an amount paid of $3 is 2 $1 bills and 2 quarters."
-print(optimal_change(10, 5) == "No change!")
-print(optimal_change(.01, .01) == "The optimal change for an item that costs $.01 with an amount paid of $.01 is none.")
+print(optimal_change(56.82, 60) == "The optimal change for an item that costs $56.82 with an amount paid of $60 is 3 $1 bills, 1 dime and 8 pennies")
+print(optimal_change(3.5, 10) == "The optimal change for an item that costs $3.5 with an amount paid of $10 is 1 $5 bill, 1 $1 bill, and 2 quarters.")
+print(optimal_change(26.90, 30) == "The optimal change for an item that costs $26.9 with an amount paid of $30 is 3 $1 bills and 1 dime.")
+print(optimal_change(.5, 3) == "The optimal change for an item that costs $0.5 with an amount paid of $3 is 2 $1 bills and 2 quarters.")
+print(optimal_change(10, 5) == 'No change!')
+print(optimal_change(.01, .01) == 'No change!')
