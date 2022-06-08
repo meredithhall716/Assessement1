1312e8ef6bec2ff750271961f29359b51c05943b
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-06-08 14:10:55 -0400

    third

--- optimal_change.py
+++ optimal_change.py
@@ -1,6 +1,3 @@
-from collections import Counter
-import math
-
 # write a function taking in two arguments: item_cost, amount_paid
 # return a string describing the least amount of change possible
 # convert item_cost and amount_paid to pennies
@@ -26,7 +23,7 @@ def optimal_change(item_cost, amount_paid):
         item_cost = item_cost * 100
         remainder = amount_paid - item_cost
        
-        pennies_list = [10000, 5000, 1000, 500, 100, 25, 10, 1]
+        pennies_list = [10000, 5000, 2000, 1000, 500, 100, 25, 10, 5, 1]
 
         # get correct change
         for i in pennies_list:
@@ -38,18 +35,21 @@ def optimal_change(item_cost, amount_paid):
                     break
         
         # print(change) = something like [100, 100, 100, 10, 1, 1, 1, 1, 1, 1, 1, 1].
-        
+        # print(change)
         
         pennies = {
             '$100 bill': 10000,
             '$50 bill': 5000,
+            '$20 bill': 2000,
             '$10 bill': 1000,
             '$5 bill': 500,
             '$1 bill': 100,
             'quarter': 25,
             'dime': 10,
+            'nickel': 5,           
             'penny': 1
         }
+        # print(pennies)
 
         # loop through the change list and the pennies dictionary to match the correct change with the correct string.
         for i in change:
@@ -62,6 +62,7 @@ def optimal_change(item_cost, amount_paid):
         
         # loop through dictionary and put it into the output string
         for key, value in counts.items():
+
             if key != list(counts.keys())[-1] and value > 1:
                 output = output + f'{value} {key}s, '
             elif key != list(counts.keys())[-1]:
@@ -70,57 +71,21 @@ def optimal_change(item_cost, amount_paid):
                 output = output + f'and {value} {key}s.'
             else:
                 output = output + f'and {value} {key}.'
-    
-    return(output)
-        
-# optimal_change(56.82, 60)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # count = 0
-        
-        # for i in answer:
-        #     if answer[0] == answer[1]:
-        #         count = count + 1
-        
-       
-       
-        # for i in pennies_list: 
-        #     if i in currency:
-        #         print(str(currency[i]))
-        #     else:
-        #         return False
-        
 
- # for key in change.keys():
-    #     if math.floor(remainder/change[key]):
-    #         remainder %= change[key]
-    #         answer.update(change[key])
-    #         # answer.append(change[key])
+        # new_output = []
+        split = output.split()
+        # for i in split:
+        #     if i == 'pennys':
+        #         new_output.append(i.replace('penny', 'pennies'))
+        # print(new_output)
 
-    #         print(answer)
-    #         print(remainder)
+        # print(split)
+        res = [item.replace('penny', 'pennies') for item in split]
+        print(res)
+        
+    print(output)
+  
+optimal_change(56.82, 60)
+# "The optimal change for an item that costs $75.75 with an amount paid of $100 is 1 $20 bill, 4 $1 bills, and 1 quarter."
+# "The optimal change for an item that costs $56.82 with an amount paid of $60 is 3 $1 bills, 1 dime, and 8 pennies"
 
-    # for i in range(len(change)):
-    #         if remainder > change[i]:
-    #             remainder = change[i] - remainder
-
-    # for values in pennies:
-    #     while remainder > pennies[values]:
-    #         if values in answer:
-    #             answer[values] += 1
-                
-    #         else:
-    #             answer[values] = 1
-    #         remainder -= pennies[values]
-       
